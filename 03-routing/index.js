cf2554f51804379617ebcc3ef1248db7321d9828
Vasil Vanchuk <v.vanchuk@tut.by>
2015-07-05 00:59:29 +0300

    Koa.js simply routing

---
+++ 03-routing/index.js
@@ -0,0 +1,19 @@
+
+var koa = require('koa');
+
+var app = module.exports = koa();
+
+app.use(function* (next) {
+  if(this.request.path !== '/') return yield next;
+  this.response.body = 'hello world';
+});
+
+app.use(function* (next) {
+  if(this.request.path !== '/404') return yield next;
+  this.response.body = 'page not found';
+});
+
+app.use(function* (next) {
+  if(this.request.path !== '/500') return yield next;
+  this.response.body = 'internal server error';
+});
