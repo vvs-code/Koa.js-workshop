8de7cb1504ccf3413127e51aafc1741200a0a092
Vasil Vanchuk <v.vanchuk@tut.by>
2015-07-05 00:43:55 +0300

    Co and Promises

---
+++ 01-co/test.js
@@ -0,0 +1,35 @@
+var co = require('co');
+var assert = require('chai').assert;
+
+var fs = require('./index.js');
+
+describe('.stats()', function () {
+  it('should stat this file', co(function* () {
+    var stats = yield fs.stat(__filename);
+    assert.ok(true);
+  }))
+
+  it('should throw on a nonexistent file', co(function* () {
+    try {
+      yield fs.stat(__filename + 'lkjaslkdjflaksdfasdf');
+      // the above expression should throw,
+      // so this error will never be thrown
+      throw new Error('nope');
+    } catch (err) {
+      assert(err.message !== 'nope');
+    }
+  }))
+})
+
+describe('.exists()', function () {
+  it('should find this file', co(function* () {
+    var a = yield fs.exists(__filename);
+    console.log('-----');
+    console.log("" + a);
+    assert.equal(true, a)
+  }));
+
+  it('should return false for a nonexistent file', co(function* () {
+    assert.equal(false, yield fs.exists(__filename + 'kljalskjdfklajsdf'))
+  }))
+})
