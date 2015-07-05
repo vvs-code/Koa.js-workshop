a7d7990e1d315f1fa40fc8f75e5b9c8d576f922b
Vasil Vanchuk <v.vanchuk@tut.by>
2015-07-05 11:26:22 +0300

    Use co.wrap to fix test on newest version of co

--- 01-co/test.js
+++ 01-co/test.js
@@ -4,12 +4,12 @@ var assert = require('chai').assert;
 var fs = require('./index.js');
 
 describe('.stats()', function () {
-  it('should stat this file', co(function* () {
+  it('should stat this file', co.wrap(function* () {
     var stats = yield fs.stat(__filename);
     assert.ok(true);
   }))
 
-  it('should throw on a nonexistent file', co(function* () {
+  it('should throw on a nonexistent file', co.wrap(function* () {
     try {
       yield fs.stat(__filename + 'lkjaslkdjflaksdfasdf');
       // the above expression should throw,
@@ -22,14 +22,14 @@ describe('.stats()', function () {
 })
 
 describe('.exists()', function () {
-  it('should find this file', co(function* () {
+  it('should find this file', co.wrap(function* () {
     var a = yield fs.exists(__filename);
     console.log('-----');
     console.log("" + a);
     assert.equal(true, a)
   }));
 
-  it('should return false for a nonexistent file', co(function* () {
+  it('should return false for a nonexistent file', co.wrap(function* () {
     assert.equal(false, yield fs.exists(__filename + 'kljalskjdfklajsdf'))
   }))
 })
